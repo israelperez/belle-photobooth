0ee2af141d3cbc929b5e33b98fe8ccf3e95208bb
Israel Perez <israel.perez@healthdirect.org.au>
2016-08-01 15:25:06 +1000

    header scripts

--- scripts/first.js
+++ scripts/first.js
@@ -1,2 +1,34 @@
-var first = true;
-window.console.log(first+'dsfdsf');
+'use strict';
+
+var UIEngine = {
+  viewport: $(window),
+
+  init: function() {
+    this.addListeners();
+    this.HEADER.updateSize();
+  },
+  addListeners: function(){
+    this.viewport.resize(function() {
+      UIEngine.HEADER.updateSize();
+    });
+  },
+  HEADER: {
+    headerElement: $('.hero_img'),
+    imageRatio: 1440/850,
+    updateSize: function() {
+      if(UIEngine.viewport.width() > 320){
+        var newHeight = UIEngine.viewport.width()/this.imageRatio,
+          maxHeight = UIEngine.viewport.height() - 52;
+        if(newHeight < maxHeight){
+          this.headerElement.height(newHeight);
+        }else{
+          this.headerElement.height(maxHeight);
+        }
+      }
+    }
+  }
+};
+
+$(document).ready(function() {
+  UIEngine.init();
+});
