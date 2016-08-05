0bac7f9b266bb2964839fb312d5a9aa0a161de20
Israel Perez <israel.perez@healthdirect.org.au>
2016-08-05 11:41:39 +1000

    added media breakpoint support and tablet menu styles and function

--- scripts/first.js
+++ scripts/first.js
@@ -2,28 +2,58 @@
 
 var UIEngine = {
   viewport: $(window),
+  breakpoint: null,
 
   init: function() {
     this.addListeners();
     this.HEADER.updateSize();
+    this._setBreakpoint();
+  },
+  _setBreakpoint: function(){
+    this.breakpoint = window.getComputedStyle(
+	     document.querySelector('body'), ':after'
+     ).getPropertyValue('content');
   },
   addListeners: function(){
     this.viewport.resize(function() {
-      UIEngine.HEADER.updateSize();
+      var viewportWidth = UIEngine.viewport.width();
+      UIEngine.HEADER.updateSize(viewportWidth);
+      UIEngine.HEADER.updateNav(viewportWidth);
+      UIEngine._setBreakpoint();
+    });
+    this.HEADER._elements.navBtn.on('click', function(){
+      UIEngine.HEADER._elements.navList.slideToggle();
+    });
+    this.HEADER._elements.navList.on('click', 'li', function(){
+      if(UIEngine.breakpoint === "mobile"){
+        UIEngine.HEADER._elements.navList.slideToggle();
+      }
     });
   },
   HEADER: {
-    headerElement: $('.hero_img'),
+    _elements: {
+      navBtn: $('.hero_nav-mob-menu'),
+      navList: $('.hero_nav-list'),
+      headerElement: $('.hero_img')
+    },
+
     imageRatio: 1440/850,
-    updateSize: function() {
-      if(UIEngine.viewport.width() > 320){
-        var newHeight = UIEngine.viewport.width()/this.imageRatio,
+    updateSize: function(width) {
+      width = width || UIEngine.viewport.width();
+
+      if(width > 320){
+        var newHeight = width / this.imageRatio,
           maxHeight = UIEngine.viewport.height() - 52;
         if(newHeight < maxHeight){
-          this.headerElement.height(newHeight);
+          this._elements.headerElement.height(newHeight);
         }else{
-          this.headerElement.height(maxHeight);
+          this._elements.headerElement.height(maxHeight);
         }
+      }
+    },
+    updateNav: function(width){
+      if(width > 760){
+        UIEngine.HEADER._elements.navList.show();
       }
     }
   }
